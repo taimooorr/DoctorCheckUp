63b940413f66d1ad5ca0db26a34a35fa0be7facc
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2022-08-11 11:10:07 +0500

    Create Oragn Class

--- src/org/thecodeguy/doctorscheckup/Organ.java
+++ src/org/thecodeguy/doctorscheckup/Organ.java
@@ -1,3 +1,31 @@
+package org.thecodeguy.doctorscheckup;
+
 public class Organ {
-    
+    private String name;
+    private String Condition;
+
+    public Organ(String name, String Condition) {
+        this.name = name;
+        this.Condition = Condition;
+    }
+    public void GetDetails() {
+        System.out.println("Organ Name: " + name);
+        System.out.println("Organ Condition: " + Condition);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCondition() {
+        return Condition;
+    }
+
+    public void setCondition(String condition) {
+        Condition = condition;
+    }
 }
